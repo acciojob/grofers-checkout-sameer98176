c42e5e88fa431c2900e0a911932e657dba7453a9
Sameer Baghel <sameerbaghel0001@gmail.com>
2026-04-23 13:46:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
+getSumBtn.innerText = "Get Total Price";
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+  let prices = document.querySelectorAll(".price");
+  let total = 0;
+
+  prices.forEach(p => {
+    total += parseFloat(p.innerText);
+  });
+  let existing = document.getElementById("ans");
+  if (existing) {
+    existing.innerText = total;
+    return;
+  }
+  let tr = document.createElement("tr");
+  let td = document.createElement("td");
+
+  td.setAttribute("colspan", "2");
+  td.setAttribute("id", "ans");
+  td.innerText = total;
+
+  tr.appendChild(td);
+  document.querySelector("table").appendChild(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
